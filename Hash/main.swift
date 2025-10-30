0a2434757bf81a40b17ae7b15171f02b7b4f950f
Vinicius Ribeiro <163923135+ribeiross@users.noreply.github.com>
2025-10-30 20:55:58 -0300

    adicionei a tal da colisão

--- Hash/main.swift
+++ Hash/main.swift
@@ -1,6 +1,4 @@
 import Foundation
-import Collections
-import DequeModule
 
 struct bucket<Key: Hashable, Value> {
     let key: Key
@@ -8,46 +6,58 @@ struct bucket<Key: Hashable, Value> {
 }
 
 struct HashTable<Key: Hashable, Value> {
-    private var buckets: [bucket<Key, Value>?]
-    
-    init(capacity: Int) {
-        self.buckets = Array(repeating: nil, count: capacity)
+    private var buckets: [[bucket<Key, Value>]]
+
+    private var capacity: Int {
+        return buckets.count
     }
-    
-    func hasher(_ key: Key) -> Int { //algoritmo pra pegar o hash da chave
+
+    init(capacity: Int) {
+        self.buckets = Array(repeating: [], count: capacity)
+    }
+
+    func hasher(_ key: Key) -> Int {  //algoritmo pra pegar o hash da chave
         var hashValue = 0
         let stringKey = "\(key)"
-        
+
         for scalar in stringKey.unicodeScalars {
             hashValue += Int(scalar.value)
         }
-        
-        return hashValue % buckets.count
+
+        return abs(hashValue % capacity)
     }
-    
+
     mutating func insert(key: Key, value: Value) {
         let index = hasher(key)
-        
-        if let existingBucket = buckets[index] {
+
+        for (i, existingBucket) in buckets[index].enumerated() {
             if existingBucket.key == key {
-                buckets[index]?.value = value
-            } else { // tem que ajeitar isso ainda. n tem suporte pra colisão
-                print("Colisão: \(index),  \(key)")
+                buckets[index][i].value = value
+                print("Atualizado \(key) no indice \(index)")
+                return
             }
+        }
+        let newBucket = bucket(key: key, value: value)
+        buckets[index].append(newBucket)
+
+        if buckets[index].count > 1 {
+            print("Adicionado \(key) no indice \(index) (Colisão resolvida)")
         } else {
-            buckets[index] = bucket(key: key, value: value)
-            return print("Added \(key) in index \(index)")
+            print("Adicionado \(key) no indice \(index)")
         }
     }
-    
-    func list(getValueIn: Key? = nil) -> String { //só pra ver oq tem na tabela/achar valor
+
+    func list(getValueIn: Key? = nil) -> String {  //só pra ver oq tem na tabela/achar valor
         if let key = getValueIn {
             let index = hasher(key)
-            if let value = buckets[index]?.value {
-                return "Value in \(key) is \(value)"
-            } else {
-                return "Bad Key: \(key)"
+            let chain = buckets[index]
+
+            for bucket in chain {
+                if bucket.key == key {
+                    return "Valor na \(key) é \(bucket.value)"
+                }
             }
+            return "Chave invalida: \(key)"
         }
         return "\(buckets)"
     }
@@ -56,8 +66,12 @@ struct HashTable<Key: Hashable, Value> {
 var Dictionary = HashTable<String, String>(capacity: 10)
 Dictionary.insert(key: "name", value: "cim borg")
 Dictionary.insert(key: "animal", value: "dog")
+Dictionary.insert(key: "mane", value: "juba")
 
-print(Dictionary.list())
+print("----------------")
 print(Dictionary.list(getValueIn: "name"))
 print(Dictionary.list(getValueIn: "animal"))
+print(Dictionary.list(getValueIn: "mane"))
 print(Dictionary.list(getValueIn: "bleh"))
+print("----------------")
+print(Dictionary.list())
