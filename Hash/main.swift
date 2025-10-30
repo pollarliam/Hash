a120a097d0258ace211800b51bebc0e2728277b1
Liam 🌸 <106059827+pollarliam@users.noreply.github.com>
2025-10-30 05:15:02 -0300

    socorro

--- Hash/main.swift
+++ Hash/main.swift
@@ -1,11 +1,51 @@
-//
-//  main.swift
-//  Hash
-//
-//  Created by Ramael Cerqueira on 29.10.2025.
-//
-
 import Foundation
+import Collections
+import DequeModule
 
-print("Hello, World!")
+struct bucket<Key: Hashable, Value> {
+    let key: Key
+    var value: Value
+}
 
+struct HashTable<Key: Hashable, Value> {
+    private var buckets: [bucket<Key, Value>?]
+    
+    init(capacity: Int) {
+        self.buckets = Array(repeating: nil, count: capacity)
+    }
+    
+     mutating func hasher(_ key: Key) -> Int {
+        var hashValue = 0
+        let stringKey = "\(key)"
+        
+        for scalar in stringKey.unicodeScalars {
+            hashValue += Int(scalar.value)
+        }
+        
+        return hashValue % buckets.count
+    }
+    
+    mutating func insert(key: Key, value: Value) {
+        let index = hasher(key)
+        
+        // Check if bucket already has something
+        if let existingBucket = buckets[index] {
+            // If key already exists, update its value
+            if existingBucket.key == key {
+                buckets[index]?.value = value
+            } else {
+                print("Colisão: \(index),  \(key)")
+            }
+        } else {
+            // Empty slot, insert new bucket
+            buckets[index] = bucket(key: key, value: value)
+            return print("Adicionado \(key) em índice \(index)")
+        }
+    }
+}
+
+var table = HashTable<String, String>(capacity: 10)
+table.insert(key: "name", value: "Pedro")
+
+print(table.hasher("name"))
+print(table)
